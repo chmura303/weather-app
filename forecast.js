bd9f0008e9cd4824c097543a65aa0c5a0036e44c
Przemyslaw Chmura <chmura303@gmail.com>
2023-06-07 11:12:42 +0200

    Fix footer

--- forecast.js
+++ forecast.js
@@ -5,7 +5,6 @@ const API = new FetchWrapper(
     ? 'http://dataservice.accuweather.com'
     : 'https://dataservice.accuweather.com'
 );
-const key = '7mrOGHxhz3eBJLYvg2hngLwMsBGxyg4T';
 
 export const getCity = city => {
   return new Promise((res, rej) => {
@@ -28,3 +27,26 @@ export const getWeather = id => {
       .catch(err => console.log(err));
   });
 };
+
+// ----- async await version -----
+
+// const API =
+//   location.protocol === 'http:'
+//     ? 'http://dataservice.accuweather.com'
+//     : 'https://dataservice.accuweather.com';
+
+// const key = '7mrOGHxhz3eBJLYvg2hngLwMsBGxyg4T';
+
+// export const getCity = async city => {
+//   const res = await fetch(
+//     `${API}/locations/v1/cities/search?apikey=${key}&q=${city}`
+//   );
+//   const data = await res.json();
+//   return data[0];
+// };
+
+// export const getWeather = async id => {
+//   const res = await fetch(`${API}/currentconditions/v1/${id}?apikey=${key}`);
+//   const data = await res.json();
+//   return data[0];
+// };
